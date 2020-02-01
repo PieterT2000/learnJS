3bf8e6e9bb1df62ce4542a02ef47c1f3dba4d4eb
PieterT2000 <info@codebirds.nl>
2020-02-01 19:49:11 +0100

    Push fully working accordion to master

--- accordion/js/main.js
+++ accordion/js/main.js
@@ -5,6 +5,12 @@ const toggleIcon = document.querySelector(".toggleIcon");
 accordions.forEach(accordion => {
   //select first child from accordion which is always the header and add click event
   accordion.firstElementChild.addEventListener("click", () => {
+    // before opening the clicked accordion, first close all other already open accordions
+    accordions.forEach(accordion => {
+      if (accordion.classList.contains("isOpen")) {
+        accordion.classList.remove("isOpen");
+      }
+    });
     accordion.classList.toggle("isOpen");
   });
 });
