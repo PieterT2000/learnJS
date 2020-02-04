402a6b4b60aff26e6dc096f37d6b56190e4a9b3c
PieterT2000 <info@codebirds.nl>
2020-02-04 10:02:52 +0100

    Push functional carousel to master

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -2,9 +2,20 @@ const slides = Array.from(document.querySelectorAll(".slide"));
 const buttonRight = document.querySelector(".right");
 const buttonLeft = document.querySelector(".left");
 
+let leftTransform = 0;
+
 buttonRight.addEventListener("click", () => {
-  slides[1].style.left = "0";
+  leftTransform += -100;
+  slides.forEach(slide => {
+    // transform each slide to the left
+    slide.style.transform = `translateX(${leftTransform}%)`;
+  });
 });
-// slides.forEach(slide => {
-//   slide.addEventListener("");
-// });
+
+buttonLeft.addEventListener("click", () => {
+  let rightTransform = leftTransform + 100;
+  slides.forEach(slide => {
+    slide.style.transform = `translateX(${rightTransform}%)`;
+  });
+  leftTransform = rightTransform;
+});
