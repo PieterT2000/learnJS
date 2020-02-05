54f1bbfba408304ad950e2d637902b323ec9eba1
PieterT2000 <info@codebirds.nl>
2020-02-05 11:49:21 +0100

    Enable event delegation + clean up code

--- accordion/js/main.js
+++ accordion/js/main.js
@@ -1,16 +1,19 @@
 // select accordions + toggle button in header
+const accordionContainer = document.querySelector(".accordionContainer");
 const accordions = document.querySelectorAll(".accordion");
 const toggleIcon = document.querySelector(".toggleIcon");
 
-accordions.forEach(accordion => {
-  //select first child from accordion which is always the header and add click event
-  accordion.firstElementChild.addEventListener("click", () => {
-    // before opening the clicked accordion, first close all other already open accordions
-    accordions.forEach(accordion => {
-      if (accordion.classList.contains("isOpen")) {
-        accordion.classList.remove("isOpen");
-      }
-    });
-    accordion.classList.toggle("isOpen");
-  });
+//Enable event delegation
+accordionContainer.addEventListener("click", e => {
+  // check if click is on accordion header or on elements inside header
+  if (
+    e.target.closest(".accordionHeader") ||
+    e.target.classList.contains("accordionHeader")
+  ) {
+    // Close currently open accordion
+    const openAccordion = accordionContainer.querySelector(".isOpen");
+    openAccordion.classList.remove("isOpen");
+    // Open accordion that is being clicked on
+    e.target.closest(".accordion").classList.add("isOpen");
+  }
 });
