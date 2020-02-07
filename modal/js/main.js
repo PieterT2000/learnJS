360bef8a63bd6bea26e5dd41792e31ab963420d8
PieterT2000 <info@codebirds.nl>
2020-02-07 09:50:56 +0100

    Add animations

--- modal/js/main.js
+++ modal/js/main.js
@@ -7,6 +7,8 @@ const body = document.body;
 //add class to body that makes modal visible on button click
 openModal.addEventListener("click", _ => {
   body.classList.add("modalIsOpen");
+  //run waving hand animation
+  waveAndZoom();
 });
 
 // close modal on click on overlay or close button
@@ -19,3 +21,24 @@ modalOverlay.addEventListener("click", e => {
     body.classList.remove("modalIsOpen");
   }
 });
+
+//Waving Hand animation on modal using GSAP
+const wavingHand = modalOverlay.querySelector(".wavingHand");
+
+const waveAndZoom = () => {
+  const tl = new TimelineMax({ repeat: -1 });
+  // Sets transform origin
+  tl.set(wavingHand, { transformOrigin: "bottom center" });
+  //first zoom out
+  tl.from(wavingHand, 0.5, {
+    scale: 0.25,
+    opacity: 0,
+    ease: Back.easeOut.config(1.5)
+  })
+    //then wave hand -> total duration 1s
+    .to(wavingHand, 0.2, { rotation: 15 })
+    .to(wavingHand, 0.2, { rotation: -15 })
+    .to(wavingHand, 0.2, { rotation: 15 })
+    .to(wavingHand, 0.2, { rotation: -15 })
+    .to(wavingHand, 0.2, { rotation: 0 });
+};
