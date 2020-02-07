c6b2f1675845c6cf1abad1fe2e477d9502a9b5cd
PieterT2000 <info@codebirds.nl>
2020-02-07 11:18:00 +0100

    Replace janky animation

--- accordion/js/main.js
+++ accordion/js/main.js
@@ -1,16 +1,36 @@
 const accordionContainer = document.querySelector(".accordionContainer");
 
+//Open first accordion by default
+function openFirstAccordion() {
+  const defaultAccordion = accordionContainer.children[0];
+  const height = defaultAccordion
+    .querySelector(".inner")
+    .getBoundingClientRect().height;
+  defaultAccordion.querySelector(
+    ".accordionContent"
+  ).style.height = `${height}px`;
+  defaultAccordion.classList.add("isOpen");
+}
+
+openFirstAccordion();
+
 //Enable event delegation
 accordionContainer.addEventListener("click", e => {
+  const accordionHeader = e.target.closest(".accordionHeader");
   // check if click is on accordion header or on elements inside header
-  if (
-    e.target.closest(".accordionHeader") ||
-    e.target.classList.contains("accordionHeader")
-  ) {
-    // Close currently open accordion
+  if (accordionHeader) {
+    //Get height of content box accordion
+    const accordionContent = e.target.closest(".accordionHeader")
+      .nextElementSibling;
+    const innerHeight = accordionContent
+      .querySelector(".inner")
+      .getBoundingClientRect().height;
+    // Get currently open accordion and close it
     const openAccordion = accordionContainer.querySelector(".isOpen");
+    openAccordion.querySelector(".accordionContent").style.height = "0px";
     openAccordion.classList.remove("isOpen");
-    // Open accordion that is being clicked on
+    // Open clicked accordion
+    accordionContent.style.height = `${innerHeight}px`;
     e.target.closest(".accordion").classList.add("isOpen");
   }
 });
