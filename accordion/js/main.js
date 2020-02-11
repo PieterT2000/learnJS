e632a372ad0fb669b8045ba3a1175a55404ac23e
PieterT2000 <info@codebirds.nl>
2020-02-11 11:54:26 +0100

    Refactor to declarative code

--- accordion/js/main.js
+++ accordion/js/main.js
@@ -1,36 +1,49 @@
-const accordionContainer = document.querySelector(".accordionContainer");
+/**
+ * Finds the correct height of the accordion content
+ * @param {HTMLElement} accordion The accordion
+ * @returns {Number} Accordion content's height in px value
+ */
+const getContentHeight = accordion => {
+  const accordionInner = accordion.querySelector(".inner");
+  return accordionInner.getBoundingClientRect().height;
+};
 
-//Open first accordion by default
-function openFirstAccordion() {
-  const defaultAccordion = accordionContainer.children[0];
-  const height = defaultAccordion
-    .querySelector(".inner")
-    .getBoundingClientRect().height;
-  defaultAccordion.querySelector(
-    ".accordionContent"
-  ).style.height = `${height}px`;
-  defaultAccordion.classList.add("isOpen");
-}
-
-openFirstAccordion();
-
-//Enable event delegation
-accordionContainer.addEventListener("click", e => {
-  // If click is outside header return early
-  const accordionHeader = e.target.closest(".accordionHeader");
-  if (!accordionHeader) return;
-
-  //Get height of content box accordion
-  const accordionContent = e.target.closest(".accordionHeader")
-    .nextElementSibling;
-  const innerHeight = accordionContent
-    .querySelector(".inner")
-    .getBoundingClientRect().height;
-  // Get currently open accordion and close it
+/**
+ * Closes current accordion when a new one is opened
+ */
+const closePreviousAccordion = () => {
   const openAccordion = accordionContainer.querySelector(".isOpen");
   openAccordion.querySelector(".accordionContent").style.height = "0px";
   openAccordion.classList.remove("isOpen");
-  // Open clicked accordion
-  accordionContent.style.height = `${innerHeight}px`;
-  e.target.closest(".accordion").classList.add("isOpen");
+};
+
+/**
+ * Opens the accordion
+ * @param {HTMLElement} accordion The clicked accordion
+ * @param {Number} height The content height of the accordion
+ */
+const openAccordion = (accordion, height) => {
+  const accordionContent = accordion.querySelector(".accordionContent");
+  accordion.classList.add("isOpen");
+  accordionContent.style.height = `${height}px`;
+};
+
+const accordionContainer = document.querySelector(".accordionContainer");
+accordionContainer.addEventListener("click", e => {
+  const accordionHeader = e.target.closest(".accordionHeader");
+  if (!accordionHeader) return;
+  const accordion = accordionHeader.parentElement;
+  const height = getContentHeight(accordion);
+  closePreviousAccordion();
+  openAccordion(accordion, height);
 });
+
+/**
+ * Opens the first accordion by default
+ */
+const openFirstAccordion = () => {
+  const accordion = accordionContainer.children[0];
+  const height = getContentHeight(accordion);
+  openAccordion(accordion, height);
+};
+openFirstAccordion();
